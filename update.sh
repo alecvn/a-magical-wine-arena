746350deca2c5432a876231df65e7aa124aa8c60
alecvn <7558545+alecvn@users.noreply.github.com>
2020-02-12 00:40:39 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Download latest version from: https://forums.mtgarena.com/forums/threads/58489
+
+WINEPREFIX=~/.PlayOnLinux/wineprefix/MTGA wine msiexec /i ~/Downloads/MTGAInstaller_0.1.2107.761453.msi
